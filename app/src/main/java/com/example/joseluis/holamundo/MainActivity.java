5ea8c60d1e3c8f65dda43dcc952d0b6eb910be0c
Unknown <fraquency@gmail.com>
2018-07-16 20:24:16 -0700

    Dibujos

--- app/src/main/java/com/example/joseluis/holamundo/MainActivity.java
+++ app/src/main/java/com/example/joseluis/holamundo/MainActivity.java
@@ -1,44 +1,42 @@
 package com.example.joseluis.holamundo;
 
-import android.animation.Animator;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.contenido_dibujitos);
 
-        findViewById(R.id.buttonChi).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Cui cui we", Toast.LENGTH_SHORT)
-                        .show();
-
-                final View lasChelasKrnal = findViewById(R.id.img_chelas);
-                lasChelasKrnal.animate()
-                        .alpha(1)
-                        .setDuration(300)
-                        .setListener(new Animator.AnimatorListener() {
-                            @Override
-                            public void onAnimationStart(Animator animation) {}
-
-                            @Override
-                            public void onAnimationEnd(Animator animation) {
-                                lasChelasKrnal.setVisibility(View.VISIBLE);
-                            }
-
-                            @Override
-                            public void onAnimationCancel(Animator animation) {}
-
-                            @Override
-                            public void onAnimationRepeat(Animator animation) {}
-                        });
-            }
-        });
+        /* Esto es código comentado, no se toma en cuenta :) cui cui we */
+//        findViewById(R.id.buttonChi).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Cui cui we", Toast.LENGTH_SHORT)
+//                        .show();
+//
+//                final View lasChelasKrnal = findViewById(R.id.img_chelas);
+//                lasChelasKrnal.animate()
+//                        .alpha(1)
+//                        .setDuration(300)
+//                        .setListener(new Animator.AnimatorListener() {
+//                            @Override
+//                            public void onAnimationStart(Animator animation) {}
+//
+//                            @Override
+//                            public void onAnimationEnd(Animator animation) {
+//                                lasChelasKrnal.setVisibility(View.VISIBLE);
+//                            }
+//
+//                            @Override
+//                            public void onAnimationCancel(Animator animation) {}
+//
+//                            @Override
+//                            public void onAnimationRepeat(Animator animation) {}
+//                        });
+//            }
+//        });
     }
 }
